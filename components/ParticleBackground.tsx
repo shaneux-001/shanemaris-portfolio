752d61bd21bd7ea8cbfbc4f958b69b8724afc7d4
Shane Maris <maris.shane@gmail.com>
2026-04-25 03:31:51 +0000

    Fix TypeScript error in particle background components

--- components/ParticleBackground.tsx
+++ components/ParticleBackground.tsx
@@ -15,7 +15,7 @@ export default function ParticleBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const particlesRef = useRef<Particle[]>([]);
   const mouseRef = useRef({ x: 0, y: 0 });
-  const animationRef = useRef<number>();
+  const animationRef = useRef<number | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
--- components/ParticleBackgroundSubtle.tsx
+++ components/ParticleBackgroundSubtle.tsx
@@ -14,7 +14,7 @@ interface Particle {
 export default function ParticleBackgroundSubtle() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const particlesRef = useRef<Particle[]>([]);
-  const animationRef = useRef<number>();
+  const animationRef = useRef<number | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
